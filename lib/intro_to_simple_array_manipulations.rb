85bc4ef8967fef1117a7028c5e0e05e3cf9b7fa8
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-10 18:50:34 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,39 +1,27 @@
-def using_push(array, string1)
-  array = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  string1 = "Niger"
-  array.push(string1)
+def using_pop(great_hits_of_the_nineties)
+  @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  @great_hits_of_the_nineties.pop
 end
 
-def using_unshift(array, string)
-  array = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  @string = "Brooklyn Heights"
-  array.unshift(@string)
+def pop_with_args(chars_in_game_of_thrones)
+  @chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
+  @chars_in_game_of_thrones.pop(2)
 end
 
-def using_pop(array1)
-  @array1 = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  @array1.pop
+def using_shift(my_favorite_cities)
+  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  @im_so_over_this_city = @my_favorite_cities.shift
 end
 
-def pop_with_args(chararray)
-  @chararry = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
-  @chararray.pop(2)
+def shift_with_args(ice_cream_brands)
+  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  @brands_removed = @ice_cream_brands.shift(2)
 end
 
-def using_shift(cityarray)
-  @cityarray = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  @im_so_over_this_city = @cityarray.shift
-end
-
-def shift_with_args(icecreamarray)
-  @icecreamarray = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  @brands_removed = @icecreamarray.shift(2)
-end
-
-def using_concat(tharray, newarray)
-  @tharray = ["raindrops on roses", "whiskers on kittens"]
-  newarray = ["mario kart", "flatiron school"]
-  @all_my_favs = @tharray.concat(newarray)
+def using_concat(my_favorite_things, more_favs)
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["mario kart", "flatiron school"]
+  @all_my_favs = @my_favorite_things.concat(more_favs)
 end
 
 def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
